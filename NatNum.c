577d817b09aceb3c7ecbf857d0b2671a778064f4
Vishal Ingle <160319996+codewithvishh@users.noreply.github.com>
2024-11-10 22:02:57 +0530

    Add files via upload

---
+++ NatNum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,t;
+    printf("enter the number");
+    scanf("%d",&a);
+    t=a*(a+1)/2;
+    printf("The sum of naural number from 1 to 10 is: %d",t);
+}
